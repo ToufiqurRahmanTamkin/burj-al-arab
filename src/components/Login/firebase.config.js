59d73aeb5316ee6a15984e77d5bc9158d58e1ac5
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-03-17 18:24:55 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCm186NCM7HbyqWMnaBL-k_tBVz2vQ4-2c",
+    authDomain: "burj-al-arab-f8402.firebaseapp.com",
+    projectId: "burj-al-arab-f8402",
+    storageBucket: "burj-al-arab-f8402.appspot.com",
+    messagingSenderId: "838216572047",
+    appId: "1:838216572047:web:4fc8f84b1cc31634a2e4fa"
+};
+export default firebaseConfig;
